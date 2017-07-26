35fb1e7dd15b8eaa46c699e48b426416cf6d7e9a
Anthony Ascencio <anthony2025@gmail.com>
2017-07-26 15:16:45 -0400

    add redux store

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,12 +1,9 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 class Cell extends Component {
-	render() {
-		return (
-			<div className="Cell">
-			</div>
-		)
-	}
+  render() {
+    return <div className="Cell" />
+  }
 }
 
 export default Cell
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import App from './App'
+
+import registerServiceWorker from './registerServiceWorker'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import rootReducer from './store/reducers'
+
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
